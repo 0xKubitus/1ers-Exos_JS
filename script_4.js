1d6798c0fbe374e1d27c005afd91e771b4e793b4
0xKubitus <n.cubilier@gmail.com>
2022-05-26 14:38:45 +0200

    script_4 finito

--- script_4.js
+++ script_4.js
@@ -20,18 +20,46 @@ const entrepreneurs = [
   { first: 'Peter', last: 'Thiel', year: 1967 }
 ];
 
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 // Question 1: 
 console.log("Q1: Filtre dans cette liste les entrepreneurs qui sont nés dans les années 70");
 
+entrepreneurs.forEach( entrepreneur => {
+  if(entrepreneur.year >= 1970) {
+      console.log (`Nom : ${entrepreneur.first} ${entrepreneur.last}, Année de naissance : ${entrepreneur.year}`)
+  }
+});
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 // Question 2: 
 console.log("Q2: Sors une array qui contient le prénom et le nom des entrepreneurs");
 
+entrepreneurs.forEach( entrepreneur => {
+  console.log (`Nom : ${entrepreneur.first} ${entrepreneur.last}`);
+});
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 // Question 3: 
 console.log("Q3: Quel âge aurait chaque inventeur aujourd'hui ?");
 
+entrepreneurs.forEach( entrepreneur =>{
+  console.log(`${entrepreneur.first} ${entrepreneur.last} : ${2022 - entrepreneur.year} ans`)
+});
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 // Question 4: 
 console.log("Q4: Trie les entrepreneurs par ordre alphabétique du nom de famille");
+
+let arrayNames = []
+
+entrepreneurs.forEach( entrepreneur =>{
+    arrayNames.push(`${entrepreneur.last} ${entrepreneur.first}`)
+});
+
+arrayNames.sort().forEach( name =>{// J'utilise la fonction sort() pour trier par ordre alphabétique
+  console.log(name);  
+})
