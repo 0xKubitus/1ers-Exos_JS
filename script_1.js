b3e62ddf38800e031d7e0a62d16992725a5105b6
0xKubitus <n.cubilier@gmail.com>
2022-05-26 10:51:21 +0200

    exo 1 OK

---
+++ script_1.js
@@ -0,0 +1,4 @@
+console.log("Bonjour monde !"); // affichera "Bonjour, monde !" dans la console
+
+let first_name = prompt("c'est quoi ton p'tit nom ?"); // demandera ton prénom avec un prompt
+  console.log(first_name); // affichera le prenom du user dans la console qd l'user aura rempli le prompt
